25ad3acfb837dfd8a8ba4c41281d9927ab26b4cf
snsebro <shayne.sebro@datadoghq.com>
2024-06-20 11:54:25 -0400

    Initial Commit

---
+++ ShayneSwiftApp/ShayneSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShayneSwiftAppApp.swift
+//  ShayneSwiftApp
+//
+//  Created by Shayne Sebro on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ShayneSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
