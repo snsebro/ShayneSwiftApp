d655bc13694ff9963977fa8827a8e455c083c1ce
snsebro <shayne.sebro@datadoghq.com>
2024-07-18 15:09:07 -0400

    Base UI Component

---
+++ ShayneSwiftApp/DDLogo.swift
@@ -0,0 +1,20 @@
+//
+//  DDLogo.swift
+//  ShayneSwiftApp
+//
+//  Created by Shayne Sebro on 6/20/24.
+//
+
+import SwiftUI
+
+struct DDLogo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DDLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        DDLogo()
+    }
+}
